adc2020dbaea3f1f97e3fc7d402fbc242b04fb9a
Sven Axelsson <sven.axelsson1@learnet.se>
2023-11-15 06:12:05 +0100

    Props

---
+++ src/Component/Props.js
@@ -0,0 +1,11 @@
+import logo from "../Img/Nr2.jpg";
+function service(props) {
+  return (
+    <>
+      <h2>{props.Title}</h2>
+      <img src={logo} alt="servicesOne" className="SeImg" />
+      <p>{props.Information}</p>
+    </>
+  );
+}
+export default service;
--- src/Component/Service.js
+++ src/Component/Service.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../Img/Nr2.jpg";
 import "../Css/ServiceStyle.css";
+import MyContent from "../Component/Props";
 
 const Services = () => {
   return (
@@ -9,12 +10,13 @@ const Services = () => {
       <h1>Service page</h1>
       <div className="services">
         <div className="card">
-          <h2>Web Development</h2>
-          <img src={logo} alt="servicesOne" className="SeImg" />
-          <p>
-            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa "
-          </p>
+          <>
+            <MyContent
+              Title="Web Devolopment"
+              Information="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in."
+            />
+          </>
+
           <div className="btnBox">
             <div className="btn">
               <NavLink
@@ -28,12 +30,12 @@ const Services = () => {
           </div>
         </div>
         <div className="card">
-          <h2>Web Development</h2>
-          <img src={logo} alt="servicesOne" className="SeImg" />
-          <p>
-            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa "
-          </p>
+          <>
+            <MyContent
+              Title="Hosting servers"
+              Information="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Libero justo laoreet sit amet cursus sit amet. Eget duis at tellus at. Mi bibendum neque egestas congue quisque egestas diam in. Dolor purus non enim."
+            />
+          </>
           <div className="btnBox">
             <div className="btn">
               <NavLink
@@ -47,12 +49,12 @@ const Services = () => {
           </div>
         </div>
         <div className="card">
-          <h2>Web Development</h2>
-          <img src={logo} alt="servicesOne" className="SeImg" />
-          <p>
-            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa "
-          </p>
+          <>
+            <MyContent
+              Title="It advice"
+              Information="vulputate dignissim suspendisse in. Nunc aliquet bibendum enim facilisis gravida. Nullam vehicula ipsum a arcu cursus vitae congue mauris. Ac auctor augue mauris augue neque gravida. Condimentum lacinia quis vel eros. Id faucibus nisl tincidunt eget.."
+            />
+          </>
           <div className="btnBox">
             <div className="btn">
               <NavLink
